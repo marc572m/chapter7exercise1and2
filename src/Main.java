5ad793bd45f4a7e0b98ff1cc5d3a23ad030fa005
marc572m <marc572m@edu.easj.dk>
2021-09-20 14:02:56 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    /*
+    Exercise 1
+
+    1. Returns array
+    2. Returns i if index at array = grape
+    3. if pine = apple then pear +1
+
+   Exercise 2
+   Output is 30
+
+     */
+
+}
